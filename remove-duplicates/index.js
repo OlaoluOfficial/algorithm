a7fa1f91b7947e2ed1453de5876f6c10c8dfd1d8
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-05 23:25:55 +0100

    add test case

--- remove-duplicates/index.js
+++ remove-duplicates/index.js
@@ -17,3 +17,12 @@ function removeDuplicates(obj) {
   });
   return result;
 }
+
+console.log(
+  removeDuplicates({
+    "432": ["A", "A", "B", "D"],
+    "53": ["L", "G", "B", "C"],
+    "236": ["L", "A", "X", "G", "H", "X"],
+    "11": ["P", "R", "S", "D"],
+  })
+);
