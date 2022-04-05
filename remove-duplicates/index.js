9a443c607c1d44d28e96e61fb9f8a092d3be0725
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-05 23:29:06 +0100

    Update

--- remove-duplicates/index.js
+++ remove-duplicates/index.js
@@ -17,34 +17,3 @@ function removeDuplicates(obj) {
   });
   return result;
 }
-
-console.log(
-  removeDuplicates({
-    "432": ["A", "A", "B", "D"],
-    "53": ["L", "G", "B", "C"],
-    "236": ["L", "A", "X", "G", "H", "X"],
-    "11": ["P", "R", "S", "D"],
-  })
-);
-
-//   {
-//     "11": ["P", "R", "S"],
-//     "53": ["C"],
-//     "236": ["L", "X", "G", "H"],
-//     "432": ["A", "B", "D"]
-//   }
-
-console.log(
-  removeDuplicates({
-    "1": ["C", "F", "G"],
-    "2": ["A", "B", "C"],
-    "3": ["A", "B", "D"],
-  })
-);
-//   {
-//     "1": ["F", "G"],
-//     "2": ["C"],
-//     "3": ["A", "B", "D"]
-//   }
-
-module.exports = removeDuplicates;
