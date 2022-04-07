a55e4772429257e85bcdc8f1c9ad29101b33f9f2
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-07 23:55:05 +0100

    add test cases

--- isolate-duplicates/test.js
+++ isolate-duplicates/test.js
@@ -7,3 +7,22 @@ describe("Find Duplicates", () => {
       "Please enter a valid string"
     );
   });
+
+  test("serial strings", () => {
+    expect(isolateDuplicates("aaaabbcdefffffffg")).toEqual([
+      "aa[aa]bbcdeff[fffff]g",
+      2,
+    ]);
+
+    expect(isolateDuplicates("abcdefg")).toEqual(["abcdefg", 0]);
+    expect(isolateDuplicates("aabbccddeeffgg")).toEqual(["aabbccddeeffgg", 0]);
+    expect(isolateDuplicates("aabccdeefgg")).toEqual(["aabccdeefgg", 0]);
+    expect(isolateDuplicates("iiijjjjooooppppp")).toEqual([
+      "ii[i]jj[jj]oo[oo]pp[ppp]",
+      4,
+    ]);
+    expect(isolateDuplicates("ssssssssssssssssssss")).toEqual([
+      "ss[ssssssssssssssssss]",
+      1,
+    ]);
+  });
