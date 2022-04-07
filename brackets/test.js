a420b87f4456c5a84c0c3d32477332ab2dc4450a
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-08 00:13:44 +0100

    add test

--- brackets/test.js
+++ brackets/test.js
@@ -1,1 +1,8 @@
 const isValid = require("./");
+
+describe("Brackets Suite", () => {
+  test("Single Character - valid inputs", () => {
+    expect(isValid("()")).toBe("valid");
+    expect(isValid("[]")).toBe("valid");
+    expect(isValid("{}")).toBe("valid");
+  });
