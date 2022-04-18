1f40eb02e69bd431da7a3f86a215d3513c296590
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-18 22:51:33 +0100

    update

--- industrial-company/index.js
+++ industrial-company/index.js
@@ -2,5 +2,10 @@ const solution = (A) => {
   let filters = 0;
   const totalPollution = A.reduce((prev, curr) => prev + curr, 0)
   let newPollution = totalPollution
-  while((totalPollution / 2) < newPollution) {}
+  while((totalPollution / 2) < newPollution) {
+    A.sort((a,b) => a - b)
+    A[A.length - 1] = A[A.length - 1] / 2;
+    newPollution = A.reduce((prev,curr) => prev + curr, 0)
+  }
 }
+console.log(solution([5,19,8,1]));
