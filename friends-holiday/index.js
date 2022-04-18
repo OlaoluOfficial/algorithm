84eea00ac5563fbb5e9082065c307665faf19bad
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-18 21:32:06 +0100

    solved

---
+++ friends-holiday/index.js
@@ -0,0 +1,24 @@
+function solution(P,S) {
+  let count = S.length;
+  let arr = [];
+  for (let i = 0; i < P.length; i++) {
+    arr[i] = S[i] - P[i];
+    arr
+  }
+
+  let remainingSeats = arr.reduce((prev, curr) => prev + curr, 0)
+  remainingSeats
+
+  P.sort((a,b) => a - b);
+  for (let i = 0; i < P.length; i++) {
+    console.log(P[i])
+    if (P[i] <= remainingSeats){
+      count--
+      count
+      remainingSeats -= P[i]
+      remainingSeats
+    } else {
+      return count;
+    }
+  }
+}
