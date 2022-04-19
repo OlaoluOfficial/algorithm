800840183ebc92f374cd6267d4c3ff742f4511ff
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-19 20:03:46 +0100

    update

--- string-multi/index.js
+++ string-multi/index.js
@@ -1,8 +1,8 @@
 function accum(s) {
-  var result = "";
-  for (var i = 0; i < s.length; i++) {
+  const result = "";
+  for (let i = 0; i < s.length; i++) {
     result += s[i].toUpperCase();
-    for (var j = 0; j < i; j++) {
+    for (let j = 0; j < i; j++) {
       result += s[i].toLowerCase();
     }
     if (i < s.length - 1) {
