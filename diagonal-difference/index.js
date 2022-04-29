5524c04ba7c6dedb5fe0a1c24e6a48a1bc1f3359
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-29 18:33:50 +0100

    update

---
+++ diagonal-difference/index.js
@@ -0,0 +1,15 @@
+function diagonalDifference(arr) {
+  // Write your code here
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr);
+    console.log(arr[j]);
+    for (let j = 0; j <= arr[0].length; j++) {     
+      console.log(arr[i], arr[i][j])
+    }
+  }
+  return result;
+}
+console.log(diagonalDifference([11,2,4],
+  [4,5,6],
+  [10,8,-12]));
