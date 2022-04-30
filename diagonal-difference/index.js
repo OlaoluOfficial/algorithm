d1339e03b5bb0ff7564fd53cf06dfca67c85403d
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-30 12:16:19 +0100

    solve

--- diagonal-difference/index.js
+++ diagonal-difference/index.js
@@ -1,15 +1,10 @@
 function diagonalDifference(arr) {
   // Write your code here
-  let result = 0;
+  let leftToRight = 0;
+  let rightToLeft = 0;
   for (let i = 0; i < arr.length; i++) {
-    console.log(arr);
-    console.log(arr[j]);
-    for (let j = 0; j <= arr[0].length; j++) {     
-      console.log(arr[i], arr[i][j])
-    }
+    leftToRight += arr[i][i];
+    rightToLeft += arr[i][arr.length - 1 - i]
   }
-  return result;
+  return Math.abs(leftToRight - rightToLeft)
 }
-console.log(diagonalDifference([11,2,4],
-  [4,5,6],
-  [10,8,-12]));
